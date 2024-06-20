fdccfaf5a656b38365dd58a82dd67666f52332ac
User <User@DESKTOP-OSEFFHS>
2024-06-20 12:20:55 +0300

    CMD Icon

---
+++ TaskManager/TaskManager/CommandLine.Designer.cs
@@ -0,0 +1,48 @@
+﻿namespace TaskManager
+{
+	partial class CommandLine
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CommandLine));
+			this.SuspendLayout();
+			// 
+			// CommandLine
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(353, 165);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.Name = "CommandLine";
+			this.Text = "Run";
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+	}
+}
