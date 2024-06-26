557f501e587c2f73b248bb0a919ccf3e9e4f91ac
Kristal <kristal.gim@mail.ru>
2024-06-26 10:02:23 +0300

    TM 2

--- TaskManager/TaskManager/CommandLine.cs
+++ TaskManager/TaskManager/CommandLine.cs
@@ -3,74 +3,101 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.IO;
 
-namespace TaskManager
+namespace TaskMenager
 {
-	public partial class CommandLine : Form
-	{
-		public ComboBox ComboBoxFileName
-		{
-			get
-			{
-				return comboBoxFilename;
-			}
-		}
-		public CommandLine()
-		{
-			InitializeComponent();
-			Load();
-		}
-		public void Load()
-		{
-			StreamReader sr = new StreamReader("ProgramList.txt");
-
-			while (!sr.EndOfStream)
-			{
-				string item = sr.ReadLine();
-				comboBoxFilename.Items.Add(item);
-			}
-
-			sr.Close();
-		}
-
-		private void buttonOK_Click(object sender, EventArgs e)
-		{
-			try
-			{
-				string text = comboBoxFilename.Text;
-				System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo(text);
-				System.Diagnostics.Process process = new System.Diagnostics.Process();
-				process.StartInfo = startInfo;
-				process.Start();
-				//if(!comboBoxFilename.Items.Contains(comboBoxFilename.Text))
-				//	comboBoxFilename.Items.Insert(0, comboBoxFilename.Text);
-				comboBoxFilename.Items.Remove(text);
-				comboBoxFilename.Text = (text);
-				comboBoxFilename.Items.Insert(0, text);
-				this.Close();
-			}
-			catch (Exception ex)
-			{
-				MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
-			}
-		}
-
-		private void comboBoxFilename_KeyPress(object sender, KeyPressEventArgs e)
-		{
-			if (e.KeyChar == (char)Keys.Enter)
-			{
-				buttonOK_Click(sender, e);
-			}
-		}
-
-		private void CommandLine_FormClosing(object sender, FormClosingEventArgs e)
-		{
-			comboBoxFilename.Focus();
-		}
-	}
+    public partial class CommandLine : Form
+    {
+        private const string DataFilePath = "combobox_data.txt";
+        public ComboBox ComboBoxFileName
+        {
+            get
+            {
+                return ComboBoxFileName;
+            }
+        }
+        public CommandLine()
+        {
+            InitializeComponent();
+            LoadComboBoxData();
+        }
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string newText = comboBoxFileName.Text;
+                //if (!string.IsNullOrEmpty(newText))
+                //{
+                //    SaveComboBoxData();
+                //}
+                System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo(comboBoxFileName.Text);
+                System.Diagnostics.Process process = new System.Diagnostics.Process();
+                process.StartInfo = startInfo;
+                process.Start();
+                // comboBoxFIleName.Items.Insert(0,comboBoxFIleName.Text);
+                comboBoxFileName.Items.Remove(newText);
+                comboBoxFileName.Text = (newText);
+                comboBoxFileName.Items.Insert(0, newText);
+                this.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void SaveComboBoxData()
+        {
+            using (StreamWriter writer = new StreamWriter(DataFilePath))
+            {
+                foreach (string item in comboBoxFileName.Items)
+                {
+                    writer.WriteLine(item);
+                }
+            }
+        }
+        private void LoadComboBoxData()
+        {
+            if (File.Exists(DataFilePath))
+            {
+                using (StreamReader reader = new StreamReader(DataFilePath))
+                {
+                    string line;
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        comboBoxFileName.Items.Add(line);
+                    }
+                }
+            }
+        }
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void buttonBrowse_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Все файлы (.)|.";
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string filePath = openFileDialog.FileName;
+                comboBoxFileName.Text = filePath;
+            }
+        }
+        private void comboBoxFIleName_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)Keys.Enter)
+            {
+                buttonOK_Click(sender, e);
+            }
+        }
+        private void CommandLine_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            comboBoxFileName.Focus();
+        }
+    }
 }
--- TaskManager/TaskManager/MainForm.cs
+++ TaskManager/TaskManager/MainForm.cs
@@ -1,130 +1,208 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Diagnostics;
-using System.IO;
-
-namespace TaskManager
+namespace TaskMenager
 {
-	public partial class MainForm : Form
-	{
-		readonly int ramFactor = 1024;
-		readonly string suffix = "kB";
-		Dictionary<int, Process> d_processes;
-		CommandLine cmd;
-		public MainForm()
-		{
-			InitializeComponent();
-			cmd = new CommandLine();
+    public partial class MainForm : Form
+    {
+        Dictionary<int, Process> d_processes;
+        readonly int ramFactor = 1024;
+        readonly string suuffix = "kB";
+        CommandLine cmd;
+        public MainForm()
+        {
+            InitializeComponent();
+            cmd = new CommandLine();
+            KDSHDJ();
+            statusStrip1.Items.Add("");
+            LoadProcesses();
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            //LoadProcesses();
+            AddNewProcesses();
+            RemoveOldProcesse();
+            UpdateExistingProcesses();
+            statusStrip1.Items[0].Text = $"Количесвто процессов:{listViewProcesses.Items.Count}";
+        }
+        void KDSHDJ()
+        {
+            listViewProcesses.Columns.Add("PID");//item
+            listViewProcesses.Columns.Add("Name");//subitem 1
+            listViewProcesses.Columns.Add("Working set");//subitem 2
+            listViewProcesses.Columns.Add("Paek working set");//subitems 3
+            listViewProcesses.Columns[0].TextAlign = HorizontalAlignment.Center;
+        }
+        void LoadProcesses()
+        {
+            //listViewProcesses.Items.Clear();
+            //Process[] processes = Process.GetProcesses();
+            //for (int i = 0; i < processes.Length; i++)
+            //{
+            //    ListViewItem item = new ListViewItem();
+            //    item.Text = processes[i].Id.ToString();
+            //    item.SubItems.Add(processes[i].ProcessName);
+            //    listViewProcesses.Items.Add(item);
+            //}
+            d_processes = Process.GetProcesses().ToDictionary(item => item.Id, item => item);
+            foreach (KeyValuePair<int, Process> i in d_processes)
+            {
+                //ListViewItem item= new ListViewItem();
+                //item.Text = i.Key.ToString();
+                //item.SubItems.Add(i.Value.ProcessName);
+                //listViewProcesses.Items.Add(item);
+                AddProcessToListView(i.Value);
+            }
+            //statusStrip1.Items[0].Text = $"Количесвто процессов:{listViewProcesses.Items.Count}";
+        }
+        void AddNewProcesses()
+        {
+            Dictionary<int, Process> d_processes = Process.GetProcesses().ToDictionary(item => item.Id, item => item);
+            foreach (KeyValuePair<int, Process> i in d_processes)
+            {
+                if (!this.d_processes.ContainsKey(i.Key))
+                {
+                    //this.d_processes.Add(i.Key, i.Value);
+                    AddProcessToListView(i.Value);
+                }
+            }
+            //statusStrip1.Items[0].Text = $"Количесвто процессов:{listViewProcesses.Items.Count}";
+        }
+        void RemoveOldProcesse()
+        {
+            this.d_processes = Process.GetProcesses().ToDictionary(item => item.Id, item => item);
+            for (int i = 0; i < listViewProcesses.Items.Count; i++)
+            {
+                //string item_name = listViewProcesses.Items[i].Name;
+                if (!d_processes.ContainsKey(Convert.ToInt32(listViewProcesses.Items[i].Text)))
+                {
+                    listViewProcesses.Items.RemoveAt(i);
+                }
+            }
+        }
+        void UpdateExistingProcesses()
+        {
+            for (int i = 0; i < listViewProcesses.Items.Count; i++)
+            {
+                int id = Convert.ToInt32(listViewProcesses.Items[i].Text);
+                // Process process = d_processes[id];
+                listViewProcesses.Items[i].SubItems[2].Text = $"{(d_processes.TryGetValue(id, out var processInfo) ? processInfo.WorkingSet64 / ramFactor : 0)} {suuffix}";
+                listViewProcesses.Items[i].SubItems[3].Text = $"{(d_processes.TryGetValue(id, out var processInfo2) ? processInfo2.PeakWorkingSet64 / ramFactor : 0)} {suuffix}";
+            }
+        }
+        void AddProcessToListView(Process process)
+        {
+            ListViewItem item = new ListViewItem();
+            item.Text = process.Id.ToString();
+            item.SubItems.Add(process.ProcessName);
+            item.SubItems.Add($"{process.WorkingSet64 / ramFactor} {suuffix}");
+            item.SubItems.Add($"{process.PeakWorkingSet64 / ramFactor} {suuffix}");
+            listViewProcesses.Items.Add(item);
+        }
+        void RomoveProcessFromListView(int pId)
+        {
+            listViewProcesses.Items.RemoveByKey(pId.ToString());
+        }
+        private void runToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CommandLine cmd = new CommandLine();
+            cmd.ShowDialog();
+        }
 
-			SetColumns();
-			statusStrip1.Items.Add("");
-			LoadProcesses();
-		}
+        int _sortColumn = -1;
+        bool _ascending = true;
+        private void listViewProcesses_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            if (e.Column == 0 || e.Column == 1)
+            {
+                if (e.Column == _sortColumn)
+                {
+                    _ascending = !_ascending;
+                }
+                else
+                {
+                    _sortColumn = e.Column;
+                    _ascending = true;
+                }
+                listViewProcesses.ListViewItemSorter = new ListViewItemComparer(_sortColumn, _ascending);
+            }
+        }
+        public class ListViewItemComparer : IComparer
+        {
+            private int _column;
+            private bool _ascending;
 
-		private void timer1_Tick(object sender, EventArgs e)
-		{
-			//LoadProcesses();
-			AddNewProcesses();
-			RemoveOldProcesses();
-			UpdateExistingProcesses();
-			statusStrip1.Items[0].Text = ($"количество  процессов: {listViewProcesses.Items.Count}");
-		}
-		void SetColumns()
-		{
-			listViewProcesses.Columns.Add("PID");
-			listViewProcesses.Columns.Add("Name");
-			listViewProcesses.Columns.Add("Working set");
-			listViewProcesses.Columns.Add("Peak working set");
-		}
-		void LoadProcesses()
-		{
-			//listViewProcesses.Items.Clear();
-			//Process[] processes = Process.GetProcesses();
-			//for (int i = 0; i < processes.Length; i++) 
-			//{
-			//    ListViewItem item = new ListViewItem();
-			//    item.Text = processes[i].Id.ToString();
-			//    item.SubItems.Add(processes[i].ProcessName);
-			//    listViewProcesses.Items.Add(item);
-			//}
-			d_processes = Process.GetProcesses().ToDictionary(item => item.Id, item => item);
-			foreach (KeyValuePair<int, Process> i in d_processes)
-			{
-				AddProcessToListView(i.Value);
-			}
-			//statusStrip1.Items[0].Text = ($"Количество процессов: {listViewProcesses.Items.Count}");
-		}
-		void AddNewProcesses()
-		{
-			Dictionary<int, Process> d_processes = Process.GetProcesses().ToDictionary(item => item.Id, item => item);
-			foreach (KeyValuePair<int, Process> i in d_processes)
-			{
-				if (!this.d_processes.ContainsKey(i.Key))
-				{
-					//this.d_processes.Add(i.Key, i.Value);
-					AddProcessToListView(i.Value);
-				}
-			}
-			//statusStrip1.Items[0].Text = ($"Количество процессов: {listViewProcesses.Items.Count}");
-		}
-		void RemoveOldProcesses()
-		{
-			this.d_processes = Process.GetProcesses().ToDictionary(item => item.Id, item => item);
-			for (int i = 0; i < listViewProcesses.Items.Count; i++)
-			{
-				//string item_name = listViewProcesses.Items[i].Name;
-				if (!d_processes.ContainsKey(Convert.ToInt32(listViewProcesses.Items[i].Text)))
-					listViewProcesses.Items.RemoveAt(i);
-			}
-		}
-		void UpdateExistingProcesses()
-		{
-			for(int i=0; i<listViewProcesses.Items.Count; i++)
-			{
-				int id = Convert.ToInt32(listViewProcesses.Items[i].Text);
-				//Process process = d_processes[id];
-				listViewProcesses.Items[i].SubItems[2].Text = $"{d_processes[id].WorkingSet64 / ramFactor} {suffix}";
-				listViewProcesses.Items[i].SubItems[3].Text = $"{d_processes[id].PeakWorkingSet64 / ramFactor} {suffix}";
-			}
-		}
-		void AddProcessToListView(Process process)
-		{
-			
-			ListViewItem item = new ListViewItem();
-			item.Text = process.Id.ToString();
-			item.SubItems.Add(process.ProcessName);
-			item.SubItems.Add($"{process.WorkingSet64/ramFactor} {suffix}");
-			item.SubItems.Add($"{process.PeakWorkingSet64/ramFactor} {suffix}");
-			listViewProcesses.Items.Add(item);
-		}
-		void RemoveProcessFromListView(int pid)
-		{
-			listViewProcesses.Items.RemoveByKey(pid.ToString());
-		}
+            public ListViewItemComparer(int column, bool ascending)
+            {
+                _column = column;
+                _ascending = ascending;
+            }
 
-		private void runToolStripMenuItem_Click(object sender, EventArgs e)
-		{
-			//CommandLine cmd = new CommandLine();
-			cmd.ShowDialog();
-		}
+            public int Compare(object x, object y)
+            {
+                ListViewItem itemX = (ListViewItem)x;
+                ListViewItem itemY = (ListViewItem)y;
+                string valueX = itemX.SubItems[_column].Text;
+                string valueY = itemY.SubItems[_column].Text;
+                if (int.TryParse(valueX, out int numX) && int.TryParse(valueY, out int numY))
+                {
+                    if (_ascending)
+                    {
+                        return numY.CompareTo(numX);
+                    }
+                    else
+                    {
+                        return numX.CompareTo(numY);
+                    }
+                }
+                else
+                {
+                    if (_ascending)
+                    {
+                        return valueY.CompareTo(valueX);
+                    }
+                    else
+                    {
+                        return valueX.CompareTo(valueY);
+                    }
+                }
+            }
+        }
+        private void MainForm_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //string filePath = "combobox_data.txt";
+            //if (File.Exists(filePath))
+            //{
+            //    File.WriteAllText(filePath, string.Empty);
+            //}
+        }
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("combobox_data.txt");
 
-		private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
-		{
-			StreamWriter sw = new StreamWriter("ProgramList.txt");
-
-			for (int i = 0; i < cmd.ComboBoxFileName.Items.Count; i++)
-			{
-				sw.WriteLine(cmd.ComboBoxFileName.Items[i]);
-			}
-
-			sw.Close();
-		}
-	}
+            for (int i = 0; i < cmd.ComboBoxFileName.Items.Count; i++)
+            {
+                sw.WriteLine(cmd.ComboBoxFileName.Items[i]);
+            }
+            sw.Close();
+        }
+    }
 }
+//System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcesses();//все запущенные процессы
+//for (int i = 0; i < processes.Length; i++)
+//{
+//    Console.WriteLine($"{processes[i].Id}\t{processes[i].MainModule.FileName}\t");
+//    //Console.WriteLine($"Name: {processes[i].ProcessName}\t");
+//    //Console.WriteLine($"PID: {processes[i].Id}\t");
+//    //Console.WriteLine($"Path: {processes[i].MainModule.FileName}\t");
+//    //Console.WriteLine();
+//}
